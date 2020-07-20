aafb68b6571ea335937a4c9639976be3506ac611
Manisha Shetti <manishashetti@Manishas-MacBook-Pro-2.local>
2020-07-20 18:46:15 -0500

    Final Changes

---
+++ WeatherPy/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,2 @@
+# Enter your API key
+gkey = "AIzaSyDwCID1895E1nFEOWZjwHVmKZWlSjigHcU"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b24aadaab930aee196029e83ff770a70"
+
+# Google API Key
+g_key = "AIzaSyDwCID1895E1nFEOWZjwHVmKZWlSjigHcU"
